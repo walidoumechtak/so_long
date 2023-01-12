b98ca014cbab32605bf021984eb0098d6e4289f6
Walid Oumechtak <woumecht@e1r1p10.1337.ma>
2023-01-12 06:52:17 +0100

    go

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: woumecht <woumecht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 13:04:55 by woumecht          #+#    #+#             */
-/*   Updated: 2023/01/11 16:16:23 by woumecht         ###   ########.fr       */
+/*   Updated: 2023/01/12 06:51:10 by woumecht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,23 +52,29 @@ int	hand_event(int key, t_long *ptr)
 	}
 	else if (key == 126)
 	{
+		ft_printf("z : %d \n", ptr->p.z);
 		y2 = ptr->img.s2 * ptr->p.z;
+		ft_printf("y2 : %d \n", y2);
 		y = y2 - 60;
-		ptr->img.s2 = y2;
-		mlx_put_image_to_window(ptr->mlx_ptr, ptr->mlx_win, ptr->img.back_img, ptr->img.s1, y);
-		mlx_put_image_to_window(ptr->mlx_ptr, ptr->mlx_win, ptr->img.ship_img, ptr->img.s1, y);
-		mlx_put_image_to_window(ptr->mlx_ptr, ptr->mlx_win, ptr->img.back_img, ptr->img.s1, y2);
-		ptr->img.s2 = y;
-		ptr->p.z = 1;
+		// ptr->img.s2 = y2;
+		mlx_put_image_to_window(ptr->mlx_ptr, ptr->mlx_win, ptr->img.back_img, ptr->img.s1 * (ptr->p.y), y);
+		mlx_put_image_to_window(ptr->mlx_ptr, ptr->mlx_win, ptr->img.ship_img, ptr->img.s1 * (ptr->p.y), y);
+		mlx_put_image_to_window(ptr->mlx_ptr, ptr->mlx_win, ptr->img.back_img, ptr->img.s1 * (ptr->p.y), y2);
+		ptr->img.s2 = 60;
+		ft_printf("y2 : %d \n", ptr->img.s2);
+		ptr->p.z = y / 60;
 	}
 	else if (key == 125)
 	{
+		ft_printf("-- %d -- \n", ptr->p.z);
 		y2 = ptr->img.s2 * ptr->p.z;
 		y = y2 + 60;
 		ptr->img.s2 = y2;
-		mlx_put_image_to_window(ptr->mlx_ptr, ptr->mlx_win, ptr->img.bottom_img, ptr->img.s1, y);
-		mlx_put_image_to_window(ptr->mlx_ptr, ptr->mlx_win, ptr->img.back_img, ptr->img.s1, y2);
-		ptr->img.s2 = y;
+		mlx_put_image_to_window(ptr->mlx_ptr, ptr->mlx_win, ptr->img.back_img, ptr->img.s1 * (ptr->p.y), y);
+		mlx_put_image_to_window(ptr->mlx_ptr, ptr->mlx_win, ptr->img.bottom_img, ptr->img.s1 * (ptr->p.y), y);
+		mlx_put_image_to_window(ptr->mlx_ptr, ptr->mlx_win, ptr->img.back_img, ptr->img.s1 * (ptr->p.y), y2);
+		ptr->img.s2 = 60;
+		ptr->p.z = y / 60;
 	}
 	
 	return (0);	
@@ -164,9 +170,10 @@ int	main(int ac, char **av)
 			return (1);
 		}
 		fill_image_addr(ptr); // fill image address ........................
-		get_cord_palyer(arr, ptr);
-		put_all_images_to_wind(ptr, arr); // put images to window ........................
 		
+		put_all_images_to_wind(ptr, arr); // put images to window ........................
+		get_cord_palyer(arr, ptr);
+		ft_printf("[%d, %d]", ptr->p.y, ptr->p.z);
 		mlx_hook(ptr->mlx_win, 2, 0, hand_event, ptr);
 		mlx_hook(ptr->mlx_win, 17, 0, ft_exit, ptr);
 		mlx_loop(ptr->mlx_ptr);
