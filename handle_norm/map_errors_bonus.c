1e6a6267ac997806c6b080e16576470973d7d6e0
Walid Oumechtak <woumecht@e1r6p11.1337.ma>
2023-01-17 20:09:17 +0100

    go

--- handle_norm/map_errors_bonus.c
+++ handle_norm/map_errors_bonus.c
@@ -6,7 +6,7 @@
 /*   By: woumecht <woumecht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 14:58:15 by woumecht          #+#    #+#             */
-/*   Updated: 2023/01/17 20:06:20 by woumecht         ###   ########.fr       */
+/*   Updated: 2023/01/17 20:08:42 by woumecht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	map_errors_bonus(t_long *ptr, char **av)
 {
-	if (check_extenstion(av) || is_5_comp_bonus(ptr->string) == 0
+	if (check_extenstion(av) == 0 || is_5_comp_bonus(ptr->string) == 0
 		|| is_ecp_exist(ptr->string) == 0 || is_only_one_p_e(ptr) == 0
 		|| is_rectangular(ptr->string) == 0
 		|| is_closed_by_walls(ptr->string) == 0 || is_there_newline(ptr) == 0)
